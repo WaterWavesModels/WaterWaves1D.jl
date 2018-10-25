a24483ca42ed9ac102f3b5a12671ddd6d03b0178
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-10-25 21:32:11 +0200

    add simple test

--- src/FModels.jl
+++ src/FModels.jl
@@ -4,9 +4,9 @@ using Plots
 using LinearAlgebra
 using FFTW
 
-include("fig.jl")
+include("times.jl")
 include("freq.jl")
 include("mesh.jl")
 include("solvers.jl")
-include("times.jl")
+include("fig.jl")
 end 
