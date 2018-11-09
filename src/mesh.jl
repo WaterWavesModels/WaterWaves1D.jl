6652fe8c455ef809f5c57871e8b197ed60aa7e0c
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2018-11-09 09:15:11 +0100

    Update src/mesh.jl
    update mesh

--- src/mesh.jl
+++ src/mesh.jl
@@ -2,15 +2,23 @@ export Mesh
 
 struct Mesh
     
-    nx   :: Int
+    N   :: Int
     xmin :: Float64
     xmax :: Float64
     dx   :: Float64
     x    :: Vector{Float64}
+    kmin :: Float64
+    kmax :: Float64
+    dk   :: Float64
+    k    :: Vector{Float64}
     
-    function Mesh( xmin, xmax, nx)
-        dx = (xmax-xmin)/nx
-        x = range(xmin, stop=xmax, length=nx+1)[1:end-1] 
-        new( nx, xmin, xmax, dx, x)
+    function Mesh( xmin, xmax, N)
+        dx = (xmax-xmin)/N
+        x = range(xmin, stop=xmax, length=N+1)[1:end-1]
+        dk = 2π/(N*dx)
+        kmin = -N/2*dk
+        kmax = (N/2-1)*dk
+        k = [range(0, length=N ÷ 2, step = dk) ; range(kmin, length=N ÷ 2, step = dk) ]
+        new( N, xmin, xmax, dx, x, kmin, kmax, dk, k)
     end
 end
