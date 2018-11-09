ffbca055413b28fb04964262debccda2b549a474
autodocs <autodocs>
2018-11-09 11:30:15 +0000

    build based on a9c97f7

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Documentation",
+    "title": "Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#DeepWaterModels.jl-Documentation-1",
+    "page": "Documentation",
+    "title": "DeepWaterModels.jl Documentation",
+    "category": "section",
+    "text": "Modules = [DeepWaterModels]\nOrder   = [:type, :function]"
+},
+
+]}
