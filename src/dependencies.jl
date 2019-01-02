72f72e50295c44fd80fd37ca2c07baa375adfe19
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2019-01-02 15:31:10 +0100

    Reorganization

--- src/dependencies.jl
+++ src/dependencies.jl
@@ -1,3 +1,9 @@
+using ProgressMeter
+using FFTW, LinearAlgebra
+using Plots
+gr()
+#pyplot()
+
 include("types.jl")
 #include("parameters.jl")
 include("times.jl")
--- src/fig.jl
+++ src/fig.jl
@@ -1,5 +1,3 @@
-using Plots
-
 export fig
 
 function create_animation( p::Problem )
