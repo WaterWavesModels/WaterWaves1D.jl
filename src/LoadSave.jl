0989134c985aeef980545bd33fc600f5ce2c2fdf
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2019-01-14 18:18:19 +0100

    Update LoadSave.jl

--- src/LoadSave.jl
+++ src/LoadSave.jl
@@ -1,14 +1,14 @@
 using JLD
 
-## Ne gere pas les param ("NamedTuple"). Chercher comment les traduire en dictionnaire, et vice-versa
-## Pour une raison etrange, il semble ne pas pouvoir enregistrer plus que 6 problemes en meme temps...
+## Ne gere pas les param ("NamedTuple"). D'où la traduction en dictionnaire, et vice-versa.
+## Pour une raison etrange, ne peut pas enregistrer Px (plan_ifft)
 function save_problem(problems::Array{Problem,1},name::String)
       problemsave=[]
       for i in range(1,size(problems)[1])
       	push!(problemsave,ProblemSave(problems[i]))
       end
 
-      save(string("../../Datas/",name,".jld"),"problemsave",problemsave)
+      save(string(name,".jld"),"problemsave",problemsave)
 end
 
 function save_problem(problem::Problem,name::String)
@@ -28,7 +28,7 @@ function load_problem(name::String,param::NamedTuple)
             for i in range(1,size(d["problemsave"])[1])
             	push!(problems,Problem(d["problemsave"][i],param))
             end
-      elseif typeof(d["problemsave"]) == Problem
+      elseif typeof(d["problemsave"]) == ProblemSave
             problems = Problem(d["problemsave"],param)
       end
       problems
@@ -41,7 +41,7 @@ function load_problem(name::String)
             for i in range(1,size(d["problemsave"])[1])
             	push!(problems,Problem(d["problemsave"][i]))
             end
-      elseif typeof(d["problemsave"]) == Problem
+      elseif typeof(d["problemsave"]) == ProblemSave
             problems = Problem(d["problemsave"])
       end
       problems
