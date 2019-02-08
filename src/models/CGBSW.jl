cb96db50e457069949f9e827f7cfccec80115fdc
Pierre Navaro <navaro@math.cnrs.fr>
2019-02-08 23:08:22 +0100

    Update CGBSW.jl

--- src/models/CGBSW.jl
+++ src/models/CGBSW.jl
@@ -6,28 +6,28 @@ export CGBSW,mapto,mapfro
 """
 mutable struct CGBSW <: AbstractModel
 
-    label   :: String
-	datasize:: Int
-    Γ   	:: Array{Float64,1}
-    Dx      :: Array{Complex{Float64},1}
-    H       :: Array{Complex{Float64},1}
-    Π⅔      :: BitArray{1}
-    ϵ 		:: Float64
-  	hnew 	:: Vector{Complex{Float64}}
-    unew    :: Vector{Complex{Float64}}
+    label    :: String
+    datasize :: Int
+    Γ        :: Array{Float64,1}
+    Dx       :: Array{Complex{Float64},1}
+    H        :: Array{Complex{Float64},1}
+    Π⅔       :: BitArray{1}
+    ϵ        :: Float64
+    hnew     :: Vector{Complex{Float64}}
+    unew     :: Vector{Complex{Float64}}
 
-    I₁   	:: Vector{Complex{Float64}}
-    I₂    	:: Vector{Complex{Float64}}
-    I₃    	:: Vector{Complex{Float64}}
+    I₁       :: Vector{Complex{Float64}}
+    I₂       :: Vector{Complex{Float64}}
+    I₃       :: Vector{Complex{Float64}}
 
-    Px      :: FFTW.FFTWPlan
+    Px       :: FFTW.FFTWPlan
 
     function CGBSW( param::NamedTuple)
 
         label = "Cheng et al."
-		datasize = 2
-		ϵ = param.ϵ
-		mesh  = Mesh(-param.L, param.L, param.N)
+        datasize = 2
+        ϵ = param.ϵ
+        mesh  = Mesh(-param.L, param.L, param.N)
         Γ = abs.(mesh.k)
         Dx    =  1im * mesh.k            # Differentiation
         H     = -1im * sign.(mesh.k)     # Hilbert transform
@@ -50,39 +50,33 @@ end
 
 function (m::CGBSW)(U::Array{Complex{Float64},2})
 
-    ldiv!(m.hnew, m.Px , U[:,1])
+    ldiv!(m.hnew, m.Px , view(U,:,1))
 
-    m.I₁  .= U[:,2]
-    m.I₁ .*= m.Γ
+    m.I₁  .= view(U,:,2) .* m.Γ
     ldiv!(m.unew, m.Px , m.I₁)
     m.unew .^= 2
     mul!(m.I₁, m.Px , m.unew)
     m.I₁ .*= m.H
 
-    m.I₂  .= U[:,1]
-    m.I₂ .*= m.Dx
+    m.I₂  .= view(U,:,1) .* m.Dx
     ldiv!(m.unew, m.Px , m.I₂)
     m.unew .*= m.hnew
     mul!(m.I₂, m.Px , m.unew)
 
-    m.I₃  .= U[:,1]
-    m.I₃ .*= m.Γ
+    m.I₃  .= view(U,:,1) .* m.Γ
     ldiv!(m.unew, m.Px, m.I₃)
     m.unew .*= m.hnew
     mul!(m.I₃ , m.Px , m.unew)
     m.I₃ .*= m.H
 
-    m.hnew  .= -U[:,2]
-    m.hnew .*= m.Dx
+    m.hnew  .= .- view(U,:,2) .* m.Dx
 
     m.I₁ .-= m.I₂
     m.I₁ .-= m.I₃
     m.I₁ .*= m.Π⅔
     m.I₁ .*= m.ϵ
 
-    U[:,2]  .= U[:,1]
-    U[:,2] .*= m.H
-    U[:,2] .+= m.I₁
+    U[:,2] .= view(U,:,1) .* m.H .+ m.I₁
     U[:,1] .= m.hnew
 
 end
@@ -102,7 +96,7 @@ end
 
 """
 function mapfro(m::CGBSW,
-	       datum ::Array{Complex{Float64},2})
+           datum ::Array{Complex{Float64},2})
 
-		   real(ifft(datum[:,1])),real(ifft(datum[:,2]))
+           real(ifft(datum[:,1])),real(ifft(datum[:,2]))
 end
