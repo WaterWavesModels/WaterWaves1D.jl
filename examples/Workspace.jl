a3cc93b61b8cb404c1962a4f9c788a5dd99488f1
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2020-03-03 18:06:04 +0100

    Nettoyage

--- examples/Workspace.jl
+++ examples/Workspace.jl
@@ -5,25 +5,37 @@
 #using ShallowWaterModels
 include("../src/dependencies.jl")
 
-#----
-param = ( μ  = 1/2,
-			ϵ  = 1,
-        	N  = 2^17,
-            L  = 3,
-            T  = 1,
-            dt = 0.0001)
 
-#init     = BellCurve(merge(param,(θ = 1,)))
-#init = Init((η = x-> 2 .^(-abs.(x.^2)) , v = x->0 .*x))
-η = x -> 2 .^(-abs.((x).^4))
-v = x -> -10*x.*η(x)
-init = Init(η,v)
+#----
+param = ( μ  = 1,
+			ϵ  = 1/1000,
+        	N  = 2^9,
+            L  = 10,
+            T  = 10,
+            dt = 0.001)
+# k=Mesh(param).k
+# x=Mesh(param).x
+# ϵ  = 1/10
+#
+# 			f= k-> exp.(-k.*k)+ϵ*(abs.(ϵ^2*k).<1 ).*(rand(Float64,size(k)).-1/2)
+# 			ζ=real(ifftshift(ifft(f(k))))
+# 			plot(x,ζ)
+# 			plot(k,f(k))
+
+init     = BellCurve(merge(param,(θ = 2,)))
+#init = Init((η = x-> exp.(-x.*x) .+ exp.(-100*x.*x)/10 , v = x->0 .*x))
+#η = x -> 2 .^(-abs.((x).^4))
+#v = x -> -10*x.*η(x)
+#init = Init(η,v)
 
 
 models = []
-#push!(models,fdBoussinesq(merge(param,(α=1,))))
-push!(models,fdBoussinesq(merge(param,(α=1/2,))))
-#push!(models,fdBoussinesq(merge(param,(α=2/5,))))
+push!(models,PseudoSpectral(merge(param,(order=1,))))
+push!(models,PseudoSpectral(merge(param,(order=2,))))
+push!(models,PseudoSpectral(merge(param,(order=3,))))
+
+#push!(models,Boussinesq(merge(param,(a=-1/3,b=1/3))))
+push!(models,WaterWaves(param))
 
 problems = []
 for model in models
@@ -41,9 +53,10 @@ end
 #----
 p = plot(layout=(2,1))
 for problem in problems
-   	fig_problem!( p, problem, .92)
+   	fig_problem!( p, problem, 10)
 end
-plot!(xlims=[(0.275,0.285) (-100000,100000 )])
+plot!(xlims=[(0,1) (-300,300 )])
+plot!(ylims=[(-0*.005,0.005) (-15,5 )])
 display(p)
 
 savefig("final.pdf"); nothing # hide
