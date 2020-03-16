33af104cf5e1779ff7cc4b6daeecd53692185fba
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2020-03-16 11:53:44 +0100

    New Init
    Allows to transfrom any vector into function

--- src/initialdata/Init.jl
+++ src/initialdata/Init.jl
@@ -2,27 +2,70 @@ export Init
 
 """
     Init(init)
-    init is a NamedTuple and should contain a value η and a value v
+    init is a NamedTuple and should contain either
+    - a function η and a function v (in this order)
+    - a Namedtuple with a function η and a function v
+    - a mesh and two vectors Vector{Complex{Float64}} or Vector{Float64} representing η(mesh.x) and v(mesh.x) (in this order)
+    - a mesh and a Namedtuple with a vector η and a vector v as above
 
-```math
-\\eta = 2^{(-|x|^\\theta)}
-```
-
-```math
-v = 0
-```
 """
 struct Init <: InitialData
 
     η
     v
 
+    function Init(η , v)
+        new( x->η(x) , x->v(x) )
+    end
+
     function Init(p :: NamedTuple)
         new( x->p.η(x) , x->p.v(x) )
     end
 
-    function Init(η , v)
+    function Init(mesh :: Mesh, η0 , v0)
+        hatv=fft(v0)
+        hatη=fft(η0)
+        k = mesh.k
+        x₀ = mesh.x[1]
+        function η( x :: Vector{Float64} )
+            η₀ = Complex.(zeros(length(x)))
+            for j = 1:length(k)
+                η₀ .+= 1/length(k)*exp.(+1im*k[j]*(x .-x₀))*hatη[j]
+            end
+            return real.(η₀)
+        end
+        function v( x :: Vector{Float64} )
+            v₀ = Complex.(zeros(length(x)))
+            for j = 1:length(k)
+                v₀ .+= 1/length(k)*exp.(+1im*k[j]*(x .-x₀))*hatv[j]
+            end
+            return real.(v₀)
+        end
+
         new( x->η(x) , x->v(x) )
     end
+
+    function Init(mesh :: Mesh, p :: NamedTuple)
+        hatv=fft(p.v)
+        hatη=fft(p.η)
+        k = mesh.k
+        x₀ = mesh.x[1]
+        function η( x :: Vector{Float64} )
+            η₀ = Complex.(zeros(length(x)))
+            for j = 1:length(k)
+                η₀ .+= 1/length(k)*exp.(+1im*k[j]*(x .-x₀))*hatη[j]
+            end
+            return real.(η₀)
+        end
+        function v( x :: Vector{Float64} )
+            v₀ = Complex.(zeros(length(x)))
+            for j = 1:length(k)
+                v₀ .+= 1/length(k)*exp.(+1im*k[j]*(x .-x₀))*hatv[j]
+            end
+            return real.(v₀)
+        end
+
+        new( x->η(x) , x->v(x) )
+        end
 
 end
