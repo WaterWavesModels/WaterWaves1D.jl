0c10e6855b7249407d9cec02bcae8c4b4191f510
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-24 14:08:25 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
-ENV["GKSwstype"]="100"
-
 push!(LOAD_PATH,"../src/")
+
+ENV["GKSwstype"]="100"
 
 using ShallowWaterModels
 using Documenter
@@ -12,15 +12,15 @@ using Literate
 DOC_OUTPUT  = joinpath(@__DIR__, "src", "examples")
 NB_OUTPUT   = joinpath(@__DIR__, "src", "notebooks")
 
-Literate.markdown(joinpath(@__DIR__, "..",  "examples/two_problems.jl"), DOC_OUTPUT)
-Literate.notebook(joinpath(@__DIR__, "..",  "examples/two_problems.jl"), NB_OUTPUT, execute=false)
-Literate.markdown(joinpath(@__DIR__, "..",  "examples/animation.jl"), DOC_OUTPUT)
-Literate.notebook(joinpath(@__DIR__, "..",  "examples/animation.jl"), NB_OUTPUT, execute=false)
+Literate.markdown(joinpath(@__DIR__, "..",  "examples","two_problems.jl"), DOC_OUTPUT, documenter=true)
+Literate.notebook(joinpath(@__DIR__, "..",  "examples","two_problems.jl"), NB_OUTPUT, execute=false)
+Literate.markdown(joinpath(@__DIR__, "..",  "examples","animation.jl"), DOC_OUTPUT, documenter=true)
+Literate.notebook(joinpath(@__DIR__, "..",  "examples","animation.jl"), NB_OUTPUT, execute=false)
 
 makedocs(modules=[ShallowWaterModels],
          doctest = false,
          authors = "Vincent Duchene",
-         format = Documenter.HTML(),
+         format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
          sitename = "ShallowWaterModels.jl",
          pages = ["Documentation" => "index.md",
                   "Code basics"   => "basics.md",
@@ -29,6 +29,5 @@ makedocs(modules=[ShallowWaterModels],
                   "Contents"      => "contents.md"])
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/WaterWavesModels/ShallowWaterModels.jl.git"
  )
