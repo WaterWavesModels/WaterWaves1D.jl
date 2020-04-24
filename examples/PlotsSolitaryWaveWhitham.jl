d6c33be9a2bc6da15356f25776218f0e9b4efe0f
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2020-04-24 10:05:21 +0200

    nettoyage

--- examples/PlotsSolitaryWaveWhitham.jl
+++ examples/PlotsSolitaryWaveWhitham.jl
@@ -1,11 +1,82 @@
 """
-Computes the solitary wave of the Whitham equation with prescribed velocity (more than 1, less than c=1.2290408)
+Computes the solitary wave of the Whitham equation with prescribed velocity
 """
 #using ShallowWaterModels
 include("../src/dependencies.jl")
 
+#---- KdV
+"""
+	`SolitaryWaveKdV()`
+A proof of concept: numerically computes the solitary wave of the KdV equation with prescribed velocity c=5
+"""
+function SolitaryWaveKdV()
+	param = ( μ  = 1,
+			ϵ  = 1,
+        	N  = 2^10,
+            L  = 10,
+			)
+	mesh = Mesh(param)
+
+	function sol(x,α)
+		2*α*sech.(sqrt(3*2*α)/2*x).^2
+	end
+
+	u₀ = SolitaryWaveWhitham(mesh, merge(param,(c=2.5,)), sol(mesh.x,1.5)+.1*exp.(-(mesh.x).^2); iterative = false, KdV = true, α=1,tol = 1e-8)
+	u₁ = SolitaryWaveWhitham(mesh, merge(param,(c=2.6,)), sol(mesh.x,1.6)+.1*exp.(-(mesh.x).^2); iterative = false, KdV = true, α=1,tol = 1e-8)
+	u₂ = SolitaryWaveWhitham(mesh, merge(param,(c=2.7,)), sol(mesh.x,1.7)+.5*exp.(-(mesh.x).^2); iterative = false, KdV = true, α=1,tol = 1e-8)
+
+	# barycentric Lagrange interpolation (4.2) in https://people.maths.ox.ac.uk/trefethen/barycentric.pdf
+	function P(X,dc,u₀,u₁,u₂)
+		(u₀/(2*(X/dc+2))-u₁/(X/dc+1)+u₂/(2*X/dc))/(1/(2*(X/dc+2))-1/(X/dc+1)+1/(2*X/dc))
+	end
+
+
+	function solve!(u₀,u₁,u₂,c,dc)
+			tu₂ = copy(u₂)
+			u₂ .= SolitaryWaveWhitham(mesh, merge(param,(c=c,)), P.(dc,dc,u₀,u₁,u₂); iterative = false, α=1, KdV = true, verbose = true, max_iter = 5, tol = 1e-10)
+			u₀ .= u₁
+			u₁ .= tu₂
+	end
+
+	for cs = range(2.8; step = 0.1, stop = 5)
+		print(string("c = ",cs,"\n"))
+		solve!(u₀,u₁,u₂,cs,0.1)
+	end
+
+	ucomp = u₂
+	uexact = sol(mesh.x,4)
+
+	plt = plot(layout=(2,2))
+
+    plot!(plt[1,1], mesh.x, [ucomp uexact];
+     title=string("c=5"),
+     label=["computed" "formula"])
+
+    plot!(plt[2,1], fftshift(mesh.k),
+     [log10.(abs.(fftshift(fft(ucomp)))) log10.(abs.(fftshift(fft(uexact))))];
+     title="frequency",
+	 label=["computed" "formula"])
+
+	 plot!(plt[1,2], mesh.x, ucomp-uexact;
+      title=string("c=5"),
+      label="difference")
+
+     plot!(plt[2,2], fftshift(mesh.k),
+      log10.(abs.(fftshift(fft(ucomp-uexact))));
+      title="frequency",
+	  label="difference")
+end
+
+
 #----
-function SWW(c)
+"""
+	`SolitaryWaveWhitham(c)`
+
+Computes the solitary wave of the Whitham equation with prescribed velocity.
+
+`c` is the velocity, and should be more between `1` and `1.2290408`.
+"""
+function SolitaryWaveWhitham(c)
  function solKdV(x,α)
    	 2*α*sech.(sqrt(3*2*α)/2*x).^2
  end
@@ -27,41 +98,6 @@ function SWW(c)
             L  = 15,
 			)
 	mesh = Mesh(param)
-
-	#
-	# function solve!(u₀,u₁,c,dc)
-	# 	while dc > 0.00001
-	# 		new_dc = dc
-	# 		@info string("c = ",c,"dc = ",dc,"\n")
-	# 		u₀[1] .= u₁[1]
-	# 		flag = 1
-	# 		while flag==1
-	# 			u₁ = SolitaryWaveWhitham(mesh, merge(param,(c=c+new_dc,)), u₁[1]+new_dc/dc*(u₁[1]-u₀[1]); iterative = true, verbose = false)
-	# 			if u₁[2]==1
-	# 				new_dc = new_dc/2
-	# 			else
-	# 				flag=0
-	# 			end
-	# 		end
-	# 		c += new_dc
-	# 		dc = new_dc
-	# 	end
-	# 	return (u₀,u₁,c,dc)
-	# end
-
-	#(u₀,u₁,c,dc)=solve!(u₀,u₁,c,dc)
-	#c=1.101
-	#dc = 0.01
-	#
-	# function solve0!(u₀,c,old_dc,new_dc)
-	# 		u₀ = SolitaryWaveWhitham(mesh, merge(param,(c=c,)), u₀[1]; iterative = true, verbose = true)
-	# end
-	#
-	# function solve1!(u₀,u₁,c,old_dc,new_dc)
-	# 		tu = copy(u₁[1])
-	# 		u₁ = SolitaryWaveWhitham(mesh, merge(param,(c=c,)), u₁[1]+new_dc/old_dc*(u₁[1]-u₀[1]); iterative = true, verbose = true)
-	# 		u₀[1] .= tu
-	# end
 
 	# barycentric Lagrange inerpolation (4.2) in https://people.maths.ox.ac.uk/trefethen/barycentric.pdf
 	function P(X,dc,u₀,u₁,u₂)
