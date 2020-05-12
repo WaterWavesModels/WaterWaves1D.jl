ced5e324acf5e10956de2d76d923b345d86f7e7a
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2020-05-12 16:35:12 +0200

    Update LoadSave.jl
    Improve loading system, with various parameters

--- src/LoadSave.jl
+++ src/LoadSave.jl
@@ -2,16 +2,15 @@ export ProblemSave, save, load
 using JLD
 struct ProblemSave
 
+    param_problem :: Dict
     model   :: Symbol
-    initial :: Symbol
-    param   :: Dict
+    param_model   :: Dict
     solver  :: Symbol
+    param_solver   :: Dict
     data    :: Data
 
-    function ProblemSave(model, initial, param, solver, data)
-
-         new( model, initial, param, solver, deepcopy(data))
-
+    function ProblemSave(param_p, model, param_m, solver, param_s, data)
+         new( param_p, model, param_m, solver, param_s, deepcopy(data))
     end
 
 end
@@ -21,53 +20,79 @@ import Base.convert
 function convert(::Type{ProblemSave}, p :: Problem )
 
     model   = Symbol(typeof(p.model))
-    param=p.param
+    solver  = Symbol(typeof(p.solver))
+
+    param_problem = p.param
     if :param in fieldnames(typeof(p.model))
-        param=merge(param,p.model.param)
+        param_model=p.model.param
+    else
+        param_model = NamedTuple()
     end
     if :kwargs in fieldnames(typeof(p.model))
         kwargs=zip(keys(p.model.kwargs),values(p.model.kwargs))
-        param=merge(param,(kwargs=kwargs,))
+        param_model=merge(param_model,(kwargs=kwargs,))
     end
-
-    initial = Symbol(typeof(p.initial))
-    param   = Dict(pairs(param))
-    solver  = Symbol(typeof(p.solver))
+    if :param in fieldnames(typeof(p.solver))
+        param_solver=p.solver.param
+    else
+        param_solver = NamedTuple()
+    end
+    param_problem = Dict(pairs(param_problem))
+    param_model   = Dict(pairs(param_model))
+    param_solver  = Dict(pairs(param_solver))
     data    = p.data
 
-
-    ProblemSave(model,initial,param,solver,data)
+    ProblemSave(param_problem, model, param_model, solver, param_solver, data)
 
 end
 
 function convert(::Type{Problem}, p :: ProblemSave)
 
-    @show param = (;p.param...)
-    @show p.model
-    @show p.initial
-    @show p.solver
-    @show p.data.datasize,p.data.datalength
+    param_p = (;p.param_problem...)
+    param_m = (;p.param_model...)
+    param_s = (;p.param_solver...)
 
     # Reconstructs the model
-    model = getfield(ShallowWaterModels, p.model)(param)
+    model = try # try with set of parameters defined by the model
+        getfield(ShallowWaterModels, p.model)(param_m)
+    catch m
+        if isa(m,AbstractModel) == false # if error, then use set of parameters defined by the problem
+            model = getfield(ShallowWaterModels, p.model)(param_p)
+        end
+    end
     if :kwargs in fieldnames(typeof(model))
-        model = getfield(ShallowWaterModels, p.model)(param;param.kwargs...)
+        model = getfield(ShallowWaterModels, p.model)(param_m;param_m.kwargs...)
     end
 
     # Reconstructs the initial data
-    mesh=Mesh(param)
+    mesh=Mesh(param_p)
     U=first(p.data.U)
     initial = Init(mesh,U[:,1],U[:,2])
 
     # Reconstructs the solver (may not work with other user-defined solvers)
-    if p.solver == :RK4_naive
-        solver = RK4_naive()
-    else
-        solver = getfield(ShallowWaterModels, p.solver)(param)
+    solver = try
+        getfield(ShallowWaterModels, p.solver)(param_s)
+    catch s
+        if isa(s,TimeSolver) == false
+            solver = try
+                getfield(ShallowWaterModels, p.solver)(model)
+            catch s
+                if isa(s,TimeSolver) == false
+                    solver = try
+                        getfield(ShallowWaterModels, p.solver)()
+                    catch s
+                        if isa(s,TimeSolver) == false
+                            @warn "Cannot set up the solver. Choose RK4."
+                            solver = RK4(model)
+                        end
+                    end
+                end
+            end
+        end
     end
 
     # Reconstructs the structure of the problem
-    pb = Problem(model, initial, param; solver = solver)
+    pb = Problem(model, initial, param_p; solver = solver)
 
     # Writes the raw data
     pb.data = p.data
