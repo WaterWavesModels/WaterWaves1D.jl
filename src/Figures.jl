96e150a5508b81940fc22598dcf9f4f311f266ac
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2020-05-14 21:20:22 +0200

    Update Figures.jl
    Provides more options

--- src/Figures.jl
+++ src/Figures.jl
@@ -2,24 +2,25 @@ export create_animation,plot_solution!,plot_solution
 using Plots;
 using ProgressMeter
 """
-	create_animation( p; name::String, ylims=(a,b), Nframes::Int )
+	create_animation( problems; name::String, kwargs... )
 
 Creates an animation showing the evolution of initial-value problems.
 
-Argument `p` is either an element or a vector, list of elements of type `Problem`.
+Argument `problems` is either an element or a collection (vector, list, etc.) of elements of type `Problem`.
 
-The animation is saved as `name.gif` if `string` is provided.
+The animation is saved as `name.gif` if `name` is provided.
 
-`ylims` allows to specifies the y axis limits.
-If nothing is provided, then the limits are determined from the initial data.
-If anything but a Tuple is provided, the axis limits evolve with the solution.
+Other keyword arguments are as follows
+- `ylims` allows to specifies the y axis limits for the surface deformation. If `nothing` is provided (default), then the limits are determined from the initial data. If anything but a `Tuple` is provided, the axis limits evolve with the solution.
+- `vlims` and `flims` are as above, but for the velocity and frequency plots.
+- `Nframes` gives the (maximal) number of frames in the animation.
+- other arguments of `plot_solution!`
 
-`Nframes` gives the (maximal) number of frames in the animation.
-
-Return `anim` an animation, which can generate a `gif` through `gif(anim, "my_name.gif", fps=15)`
+Return `anim` an animation, which can generate for instance a `gif` through `gif(anim, "my_name.gif", fps=15)`.
 
 """
-function create_animation( problems; name=nothing, ylims=nothing, Nframes = 201 )
+function create_animation( problems; name=nothing, x = nothing, ylims=nothing,vlims=nothing,flims=nothing, Nframes = 201,
+							surface=true,frequency=true, velocity=false )
 	label=nothing
 	if isa(problems,Problem) # if create_animation is called with a single problem
 		problems=[problems];  # A one-element array to allow `for pb in p`
@@ -32,15 +33,41 @@ function create_animation( problems; name=nothing, ylims=nothing, Nframes = 201 
 		y0 = minimum(η);y1 = maximum(η);
 		ylims=(y0-(y1-y0)/10,y1+(y1-y0)/10)
 	end
+	if vlims == nothing
+		(η,v) = solution(problems[1];t=0)
+		y0 = minimum(v);y1 = maximum(v);
+		vlims=(y0-(y1-y0)/10,y1+(y1-y0)/10)
+	end
+	if flims == nothing
+		(η,v) = solution(problems[1];t=0)
+		f=log10.(abs.(fft(η)))
+		y0 = minimum(f);y1 = maximum(f);
+		flims=(y0-(y1-y0)/10,y1+(y1-y0)/10)
+	end
+
+
 	Ns=problems[1].times.Ns
 	L=range(1,stop=Ns,step=max(1,ceil(Int,Ns/Nframes)))
 	prog = Progress(length(L);dt=1,desc="Creating animation: ")
+	m = surface + frequency + velocity
     anim = @animate for l in L
-		plt = plot(layout=(2,1))
+		plt = plot(layout=(m,1))
 		for pb in problems
-			plot_solution!( plt, pb; t=pb.times.ts[l] )
+			plot_solution!( plt, pb; t=pb.times.ts[l],x=x,surface=surface,frequency=frequency, velocity=velocity )
 		end
-		if isa(ylims,Tuple) ylims!(plt[1,1],ylims) end
+		n=1
+		if surface == true
+			if isa(ylims,Tuple) ylims!(plt[n,1],ylims) end
+			n+=1
+		end
+		if velocity == true
+			if isa(vlims,Tuple) ylims!(plt[n,1],vlims) end
+			n+=1
+		end
+		if surface == true
+			if isa(flims,Tuple) ylims!(plt[n,1],flims) end
+			n+=1
+		end
         next!(prog)
     end
 	if label!=nothing problems[1].model.label=label end # puts back the model's name
@@ -49,50 +76,72 @@ function create_animation( problems; name=nothing, ylims=nothing, Nframes = 201 
 end
 
 """
-	plot_solution!( plt; problems; t,x )
+	plot_solution!( plt; problems; t,x,surface,velocity,frequency )
 
 Plots in `plt` the solution of initial-value problems at a given time.
 
-Argument `problems` is either an element or a vector or list of elements of type `Problem`.
+# Argument
+`problems` is either an element or a collection (vector, list, etc.) of elements of type `Problem`.
 
-Keyword `t` is the time. If not provided, then the last computed time is plotted.
-
-If a vector `x` is provided and if it is possible, the solution is interpolated to the collocation points given by `x`.
+## Keywords
+- `t` is the time. If not provided, then the last computed time is plotted.
+- if a vector `x` is provided and if possible, the solution is interpolated to the collocation points `x`.
+- `surface`, `velocity` and `frequency` (booleans) determine respectively whether surface deformation, `η`, tangential velocity, `v`, and the Fourier coefficients of `η` (in log-scale) are plotted.
 
 """
-function plot_solution!( plt, problems; t=nothing,x=nothing )
+function plot_solution!( plt, problems; t=nothing,x=nothing,surface=true, frequency=true, velocity=false )
 	if typeof(problems)==Problem problems=[problems] end
 	for p in problems
 		if x != nothing
-			(ηfull,vfull,xfull) = solution(p)
-			fftη = fft(ηfull)
-			(η,v,X,t) = solution(p;t=t,x=x)
-			flag=true
-		else
-			(η,v,X,t) = solution(p;t=t,x=x)
+			(η,v) = solution(p)
 			fftη = fft(η)
+			fftv = fft(v)
+			(η,v,X,t) = solution(p;t=t,x=x)
+		else
+			(η,v,X,t) = solution(p;t=t)
+			fftη = fft(η)
+			fftv = fft(v)
 		end
 
-	    plot!(plt[1,1], X, η;
-			  title=string("elevation at t=",t),
+		n=1
+		if surface == true
+	    	plot!(plt[n,1], X, η;
+			  title=string("surface deformation at t=",t),
 		      label=p.model.label)
+			n+=1
+		end
 
-	    plot!(plt[2,1], fftshift(p.mesh.k),
-	        log10.(1e-18.+abs.(fftshift(fftη)));
-			  title="Fourier modes",
+	  	if velocity == true
+	    	plot!(plt[n,1], X, v;
+			  title=string("tangential velocity at t=",t),
+		      label=p.model.label)
+			n+=1
+		end
+
+		if frequency == true
+			if surface + velocity == 1
+				f = surface * fftη + velocity * fftv
+			else
+				f = fftη
+			end
+	    	plot!(plt[n,1], fftshift(p.mesh.k),
+	          log10.(1e-20.+abs.(fftshift(f)));
+			  title="Fourier coefficients",
+			  yaxis="log scale",
 	    	  label=p.model.label)
+		end
 	end
 	nothing
 end
 
 """
-	plot_solution( problems; t,x )
+	plot_solution( problems; t,x, surface,velocity,frequency )
 
 Same as `plot_solution!` but generates and returns the plot.
 """
-function plot_solution( problems; t=nothing,x=nothing )
-	plt = plot(layout=(2,1))
-	plot_solution!( plt, problems; t=t,x=x )
+function plot_solution( problems; t=nothing,x=nothing, surface=true, velocity=false, frequency=true, )
+	plt = plot(layout=(1+frequency+velocity,1))
+	plot_solution!( plt, problems; t=t,x=x, surface=surface,frequency=frequency, velocity=velocity )
 	return plt
 end
 
