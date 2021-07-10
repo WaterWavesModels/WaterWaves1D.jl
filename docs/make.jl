41976a75d4c57142d915693168e917e780160673
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-07-10 23:59:12 +0200

    fix documentation

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,10 @@ push!(LOAD_PATH,"../src/")
 
 ENV["GKSwstype"]="100"
 
+using Pkg
+Pkg.develop(PackageSpec(path=pwd()))
+Pkg.instantiate()
+
 using ShallowWaterModels
 using Documenter
 using Plots 
@@ -12,10 +16,10 @@ using Literate
 DOC_OUTPUT  = joinpath(@__DIR__, "src", "examples")
 NB_OUTPUT   = joinpath(@__DIR__, "src", "notebooks")
 
-Literate.markdown(joinpath(@__DIR__, "..",  "examples","two_problems.jl"), DOC_OUTPUT, documenter=true)
-Literate.notebook(joinpath(@__DIR__, "..",  "examples","two_problems.jl"), NB_OUTPUT, execute=false)
-Literate.markdown(joinpath(@__DIR__, "..",  "examples","animation.jl"), DOC_OUTPUT, documenter=true)
-Literate.notebook(joinpath(@__DIR__, "..",  "examples","animation.jl"), NB_OUTPUT, execute=false)
+# Literate.markdown(joinpath(@__DIR__, "..",  "examples","two_problems.jl"), DOC_OUTPUT, documenter=true)
+# Literate.notebook(joinpath(@__DIR__, "..",  "examples","two_problems.jl"), NB_OUTPUT, execute=false)
+# Literate.markdown(joinpath(@__DIR__, "..",  "examples","animation.jl"), DOC_OUTPUT, documenter=true)
+# Literate.notebook(joinpath(@__DIR__, "..",  "examples","animation.jl"), NB_OUTPUT, execute=false)
 
 makedocs(modules=[ShallowWaterModels],
          doctest = false,
@@ -24,9 +28,10 @@ makedocs(modules=[ShallowWaterModels],
          sitename = "ShallowWaterModels.jl",
          pages = ["Documentation" => "index.md",
                   "Code basics"   => "basics.md",
-                  "Animation"     => "examples/animation.md",
-                  "Example"       => "examples/two_problems.md",
                   "Contents"      => "contents.md"])
+
+#                  "Animation"     => "examples/animation.md",
+#                 "Example"       => "examples/two_problems.md",
 
 deploydocs(
     repo   = "github.com/WaterWavesModels/ShallowWaterModels.jl.git"
