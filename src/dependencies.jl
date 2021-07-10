1cd70d856c74e31427fe72a81a7a8f45907958ab
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-07-10 22:32:14 +0200

    commit pierre 1

--- src/dependencies.jl
+++ src/dependencies.jl
@@ -1,41 +1,52 @@
+export include_all
+
 using ProgressMeter
-using FFTW, LinearAlgebra
-using LinearMaps,IterativeSolvers
-using Statistics #only for function mean in WaterWaves.jl...
-using JLD #only for LoadSave
+using FFTW
+#using LinearAlgebra
 ENV["PLOTS_USE_ATOM_PLOTPANE"] = "false"
 # Note that javascript-based libraries (for example: PlotlyJS) cannot be shown in the PlotPane due to issues within Atom's internals.
-using Plots
-gr()
+#using Plots
+#gr()
 #pyplot()
 #plotlyjs()
 
 include("types.jl")
-
 include("solvers/RK4.jl")
-include("solvers/RK4_naive.jl")
-
-include("initialdata/Init.jl")
-include("initialdata/BellCurve.jl")
-include("initialdata/HighFreq.jl")
-include("initialdata/Random.jl")
-include("initialdata/SolitaryWaveWhitham.jl")
-include("initialdata/SolitaryWaveWhithamBoussinesq.jl")
-include("initialdata/SolitaryWaveWhithamGreenNaghdi.jl")
-include("initialdata/CnoidalWaveWhithamGreenNaghdi.jl")
-
-include("models/CGBSW.jl")
-include("models/CGBSW_naive.jl")
-include("models/Matsuno.jl")
-include("models/Matsuno_naive.jl")
-include("models/Matsuno_mod_naive.jl")
-include("models/Boussinesq.jl")
-include("models/PseudoSpectral.jl")
-include("models/WaterWaves.jl")
-include("models/WhithamBoussinesq.jl")
-include("models/WhithamGreenNaghdi.jl")
-include("models/WhithamGreenNaghdiGPU.jl") #comment this line if you have problems with CuArrays
-
-include("LoadSave.jl")
-include("fig.jl")
 include("tools.jl")
+
+
+function include_all(dir=nothing)
+    if dir!=nothing
+        dir=string("src/",dir)
+        for file in readdir(abspath(dir),join=true)
+            try include(file)
+                @info string("included ",file)
+            catch
+                @info string("did not include ",file)
+            end
+        end
+    else
+        include_all("solvers")
+        include_all("initialdata")
+        include_all("models")
+        include(abspath("src/Figures.jl"))
+        include(abspath("src/LoadSave.jl"))
+    end
+end
+
+#include_all()
+
+# include("solvers/RK4.jl")
+# include("solvers/RK4_naive.jl")
+# include("models/CGBSW.jl")
+# include("models/CGBSW_naive.jl")
+# include("models/Matsuno.jl")
+# include("models/Matsuno_naive.jl")
+# include("models/Matsuno_mod_naive.jl")
+# include("models/Boussinesq.jl")
+# include("models/PseudoSpectral.jl")
+# include("models/WaterWaves.jl")
+# include("models/WhithamBoussinesq.jl")
+# include("models/WhithamGreenNaghdi.jl")
+# include("models/WhithamGreenNaghdiGPU.jl") #comment this line if you have problems with CuArrays
+#
