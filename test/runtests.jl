21bf1301ebc7d7fd4ed8725455e800694283db8d
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2021-12-23 17:46:49 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,25 +3,25 @@ ENV["GKSwstype"]="100"
 using Test
 using WaterWaves1D
 
-param = ( ϵ  = 1/2, μ = 1, θ = 1)
+param = ( ϵ  = 1/2, μ = 1)
 paramX= ( N  = 2^8, L  = 10)
 paramT= ( T  = 5, dt = 0.1)
 
-init     = BellCurve(param)
-model    = WhithamGreenNaghdi(merge(param,paramX);ktol=1e-10)
+init     = Init(x->exp.(-x.^2),x->0 .*x )
+model    = WaterWaves(merge(param,paramX);verbose=false)
 problem1 = Problem( model, init, merge(paramT,paramX); solver = RK4(paramX) )
 
 # @testset "LoadSave" begin
-# 
+#
 #     dump  = convert( ProblemSave, problem1 )
 #     pload = convert( Problem, dump )
-# 
+#
 #     save(problem1, "testsave")
 #     pload = loadpb("testsave")
-# 
+#
 #     @test pload.model.kwargs == problem1.model.kwargs
 #     @test pload.solver.Uhat == problem1.solver.Uhat
-# 
+#
 # end
 
 @testset "Parameters" begin
@@ -32,19 +32,18 @@ problem1 = Problem( model, init, merge(paramT,paramX); solver = RK4(paramX) )
     @test paramX.L  == 10
     @test paramT.T  == 5
     @test paramT.dt == 0.1
-    @test param.θ == 1
 
 end
 
 solve!(problem1)
 
-@testset "Test problem with Whitham-Green-Naghdi model" begin
+@testset "Test problem with water waves model" begin
     @test !any(isnan,problem1.data.U[end][1])
     @test !any(isnan,problem1.data.U[end][2])
 end
 
-WW3  = PseudoSpectral(merge(param,paramX);order=3,dealias=1)
-problem2 = Problem(WW3, init, merge(paramX,paramT) )
+WW2  = PseudoSpectral(merge(param,paramX);δ=0.01,order=2,dealias=1,verbose=false)
+problem2 = Problem(WW2, init, merge(paramX,paramT) )
 
 solve!( problem2 )
 
