f6512c8caa867f06ec1111dfb5c7fbdf1bac464f
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2021-12-23 17:46:43 +0100

    Nettoyage des données initiales

--- src/initialdata/Random.jl
+++ src/initialdata/Random.jl
@@ -1,9 +1,18 @@
 export Random
-
+using FFTW
 """
-    Random(param)
+    Random(;args)
 
-param should contain s (regularity index)
+Randomly generated initial data, based on provided (optional arguments) :
+- `L` is the typical wavelength (default is `L=1`)
+- `s` is the (real) Sobolev index regularity (default is `s=∞`)
+- `λ` is the length of spatial localization (default is none)
+
+The initial data `(η,v)` are generated through randomly chosen Fourier coefficients,
+multiplied with weigth `w=min(1,|k/L|^(s+1/2))` or `w=min(1,exp(-|k/L|)` if `s=∞`.
+If `λ` is provided, the function in spatial variables is multiplied by `exp(-|x/λ|^2)`,
+and in any case normalized to have maximum absolute value 1.
+
 
 """
 struct Random <: InitialData
@@ -11,26 +20,33 @@ struct Random <: InitialData
     η
     v
 
-    function Random(p :: NamedTuple)
+    function Random(;L=1,s=Inf,λ=2)
 
-        function η( x :: Vector{Float64} )
-            h = rand(Float64,size(x))
 
-            for i in 1:p.s
-                h .= cumsum(h.-sum(h)/length(h))
+        function generate( x )
+
+
+            k = Mesh( x ).k
+            if s == Inf
+                w = 1 ./ exp.(log(10)*abs.(k*L/(2*π)))
+            else
+                w = 1 ./( 1 .+ 9*abs.(k*L/(2*π)).^(s+1/2) )
+            end
+            if λ == nothing
+                φ = zero(x).+1
+            else
+                φ = exp.(-abs.((x/λ).^2))
             end
 
-            return h ./ maximum(abs.(h))
+            θ = 2*π*rand(Float64,length(x))
+            r = rand(Float64,length(x))
+            Fourier = r.*exp.(-1im.*θ).*w
+            Physic = real.(ifft(Fourier)).*φ
+            return Physic./maximum(abs.(Physic))
         end
-        function v( x :: Vector{Float64} )
-            h = rand(Float64,size(x))
+        η( x ) = generate( x )
+        v( x ) = generate( x )
 
-            for i in 1:p.s
-                h .= cumsum(h.-sum(h)/length(h))
-            end
-
-            return h ./ maximum(abs.(h))
-        end
 
     	new( η,v )
 
