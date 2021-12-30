917c1c03648de0aebd19b2c057f3971c7cd9da4e
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2021-12-30 21:12:16 +0100

    Update Figures.jl
    New functions (and slight debug)

--- src/Figures.jl
+++ src/Figures.jl
@@ -1,4 +1,4 @@
-export create_animation,plot_solution!,plot_solution
+export create_animation,plot_solution!,plot_solution,plot_difference!,plot_difference
 using Plots;
 using ProgressMeter
 """
@@ -90,12 +90,13 @@ Plots in `plt` the solution of initial-value problems at a given time.
 # Argument
 `problems` is either an element or a collection (vector, list, etc.) of elements of type `Problem`.
 
-## Keywords
+## Keyword arguments (all optional)
 - `t` is the time. If not provided, then the last computed time is plotted.
 - if a vector `x` is provided and if possible, the solution is interpolated to the collocation points `x`.
 - if `interpolation` is provided as an integer, the solution is interpolated on as many collocation points (if `true`, then the value `2^3` is chosen, default is `false`).
 - if `compression` is provided as an integer `m`, only one in `m` points are plotted (if `true`, then the value `2^3` is chosen, default is `false`).
 - `surface`, `velocity` and `fourier` (booleans) determine respectively whether surface deformation, `η`, tangential velocity, `v`, and the Fourier coefficients of `η` (in log-scale) are plotted.
+- `label` defines the label(s) if provided (otherwise labels are inferred from the models)
 
 """
 function plot_solution!( plt, problems; t=nothing,x=nothing,interpolation=false,compression=false, surface=true, fourier=true, velocity=false, label=nothing)
@@ -109,15 +110,14 @@ function plot_solution!( plt, problems; t=nothing,x=nothing,interpolation=false,
 			lbl=label[i]
 		end
 		# retrieve the solution to be plotted
-		if x != nothing  # interpoate the solution to collocation points x
-			(η,v) = solution(p)
-			fftη = fft(η)
-			fftv = fft(v)
+		if x != nothing  # interpolate the solution to collocation points x
+			(η,v) = solution(p;t=t)
+			fftη = fft(η); fftv = fft(v)
 			(η,v,X,t) = solution(p;t=t,x=x)
 		else
+			(η,v) = solution(p;t=t)
+			fftη = fft(η); fftv = fft(v)
 			(η,v,X,t) = solution(p;t=t,interpolation=interpolation)
-			fftη = fft(η)
-			fftv = fft(v)
 		end
 		# generate the indices to be plotted (all if compression = false)
 		if compression == false
@@ -164,5 +164,102 @@ Same as `plot_solution!` but generates and returns the plot.
 function plot_solution( problems; t=nothing,x=nothing,interpolation=false,compression=false, surface=true, velocity=false, fourier=true, label=nothing)
 	plt = plot(layout=(surface+fourier+velocity,1))
 	plot_solution!( plt, problems; t=t,x=x, interpolation=interpolation, compression=compression, surface=surface,fourier=fourier, velocity=velocity, label=label )
+	return plt
+end
+
+"""
+	plot_difference!( plt, pairs ; t,x,interpolation,compression,fast,surface,velocity,fourier,label )
+
+Plots in `plt` the difference between solutions of initial-value problems at a given time.
+
+# Argument
+`pairs` is a either a pair or a collection (vector, list, etc.) of pairs of type `Problem`.
+
+## Keyword arguments (all optional)
+- `t` is the time. If not provided, then the last computed time is plotted.
+- if a vector `x` is provided and if possible, the solution is interpolated to the collocation points `x`.
+- if `interpolation` is provided as an integer, the solution is interpolated on as many collocation points (if `true`, then the value `2^3` is chosen, default is `false`).
+- if `fast = false` (default is `false`), then the second problem is interpolated to the collocation points of the first problem.
+- if `compression` is provided as an integer `m`, only one in `m` points are plotted (if `true`, then the value `2^3` is chosen, default is `false`).
+- `surface`, `velocity` and `fourier` (booleans) determine respectively whether surface deformation, `η`, tangential velocity, `v`, and the Fourier coefficients of `η` (in log-scale) are plotted.
+- `label` defines the label(s) if provided (otherwise labels are inferred from the models)
+
+"""
+function plot_difference!( plt, pairs; t=nothing,x=nothing,interpolation=false,compression=false, fast=false, surface=true, fourier=false, velocity=false, label=nothing)
+	if typeof(pairs[1])==Problem pairs=[pairs] end
+	for i in 1:length(pairs)
+		p1 = pairs[i][1]
+		p2 = pairs[i][2]
+		# retrieve the label
+		if label == nothing || label == [nothing]
+			lbl=string(p1.model.label,"-",p2.model.label)
+		else
+			lbl=label[i]
+		end
+		# retrieve the solution to be plotted
+		if x != nothing  # interpolate the solution to collocation points x
+			(η1,v1) = solution(p1;t=t); (η2,v2) = solution(p2;t=t);
+			fftη1 = fft(η1); fftv1 = fft(v1); fftη2 = fft(η2); fftv2 = fft(v2);
+			(η1,v1,X1,t) = solution(p1;t=t,x=x)
+			(η2,v2,X2,t) = solution(p2;t=t,x=x)
+
+		else
+			(η1,v1) = solution(p1;t=t); (η2,v2) = solution(p2;t=t);
+			fftη1 = fft(η1); fftv1 = fft(v1); fftη2 = fft(η2); fftv2 = fft(v2);
+			(η1,v1,X1,t) = solution(p1;t=t,interpolation=interpolation)
+			if fast==true #will not interpolate to X1 if fast == true
+				(η2,v2,X2,t) = solution(p2;t=t,interpolation=interpolation)
+			else
+				(η2,v2,X2,t) = solution(p2;t=t,x=X1)
+			end
+		end
+		if X1!=X2
+			@warn string("you are comparing solutions at different collocation points!\nMaximal difference: ",maximum(abs.(X1-X2)))
+		end
+		# generate the indices to be plotted (all if compression = false)
+		if compression == false
+			compression = 1
+		elseif compression === true
+			compression = 2^3
+		end
+		indices=1:Int(compression):length(X1)
+		n=1
+		# plot the surface deformation
+		if surface
+	    	plot!(plt[n,1], X1[indices], (η1-η2)[indices];
+			  title=string("surface deformation at t=",t),
+		      label=lbl)
+			n+=1
+		end
+		# plot the velocity
+	  	if velocity
+	    	plot!(plt[n,1], X1[indices], (v1-v2)[indices];
+			  title=string("tangential velocity at t=",t),
+		      label=lbl)
+			n+=1
+		end
+		# plot the discrete fourier coefficients (in log scale)
+		if fourier
+			if !all(isnan,fftη1-fftη2)
+	    		plot!(plt[n,1], fftshift(p1.mesh.k)[indices],
+	          		eps(1.).+abs.(fftshift(fftη1-fftη2))[indices];
+			  		title="Fourier coefficients (log scale)",
+			  		#yaxis="log scale",
+			  		yscale=:log10,
+	    	  		label=lbl)
+			end
+		end
+	end
+	nothing
+end
+
+"""
+	plot_difference( pairs; t,x, interpolation,compression, surface,velocity,fourier, label )
+
+Same as `plot_difference!` but generates and returns the plot.
+"""
+function plot_difference( pairs; t=nothing,x=nothing,interpolation=false,compression=false, fast=false, surface=true, velocity=false, fourier=false, label=nothing)
+	plt = plot(layout=(surface+fourier+velocity,1))
+	plot_difference!( plt, pairs; t=t,x=x, interpolation=interpolation, compression=compression, fast=fast, surface=surface,fourier=fourier, velocity=velocity, label=label )
 	return plt
 end
