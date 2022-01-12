833186ff28eee011889ad4efe4ece4d124e2d917
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2022-01-12 13:57:31 +0100

    Update WaterWaves1D.jl
    Now all dependencies are in the main module

--- src/WaterWaves1D.jl
+++ src/WaterWaves1D.jl
@@ -1,5 +1,50 @@
 module WaterWaves1D
 
-include("dependencies.jl")
+# external modules
+using ProgressMeter,FFTW
+using LinearMaps,IterativeSolvers,LinearAlgebra
+
+# abstract types
+export AbstractModel,TimeSolver,InitialData
+abstract type AbstractModel end
+abstract type TimeSolver end
+abstract type InitialData end
+
+# sructures
+include("data.jl")
+include("mesh.jl")
+include("times.jl")
+include("init.jl")
+include("problem.jl")
+
+# tools
+include("tools.jl")
+include("figures.jl")
+
+# initial data, models, solvers
+include("initialdata/Random.jl")
+include("initialdata/CnoidalWaveSerreGreenNaghdi.jl")
+include("initialdata/SolitaryWaveSerreGreenNaghdi.jl")
+include("initialdata/SolitaryWaveWhithamGreenNaghdi.jl")
+include("initialdata/SolitaryWaveWhithamBoussinesq.jl")
+include("initialdata/SolitaryWaveWhitham.jl")
+
+include("models/Boussinesq.jl")
+include("models/DeepQuadratic.jl")
+include("models/IsobeKakinuma.jl")
+include("models/Matsuno.jl")
+include("models/NonHydrostatic.jl")
+include("models/SerreGreenNaghdi.jl")
+include("models/SquareRootDepth.jl")
+include("models/WaterWaves.jl")
+include("models/WhithamBoussinesq.jl")
+include("models/WhithamGreenNaghdi.jl")
+include("models/WWn.jl")
+
+include("solvers/Euler.jl")
+include("solvers/EulerSymp.jl")
+include("solvers/RK4_naive.jl")
+include("solvers/RK4.jl")
+
 
 end
