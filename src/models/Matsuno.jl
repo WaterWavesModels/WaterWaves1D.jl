44a7d82cccc0db40f62bdcca4cafb07e03a73d19
Vincent Duchene <vincent.duchene@univ-rennes1.fr>
2022-02-22 15:41:44 +0100

    Update Matsuno.jl
    The model is now defined in the finite-depth situation

--- src/models/Matsuno.jl
+++ src/models/Matsuno.jl
@@ -14,32 +14,61 @@ mutable struct Matsuno_fast <: AbstractModel
 	info	:: String
 
     function Matsuno_fast(param::NamedTuple;
-							dealias=false, label = "Matsuno" )
+							IL	    = false,
+							dealias=false,
+							label = "Matsuno" )
 
-			# Set up
-			ϵ 	= param.ϵ
-			mesh = Mesh(param)
-
-			# Print information
-			info = "Matsuno model.\n"
-			info *= "├─Steepness parameter ϵ=$ϵ (infinite depth case).\n"
-			if dealias == true || dealias == 1
-				info *= "└─Dealiasing with Orszag’s 3/2 rule. "
+		# Set-up
+		μ 	= param.μ
+		ϵ 	= param.ϵ
+		if !in(:ν,keys(param))
+			if μ > 1
+				ν = 1/sqrt(μ)
+				nu = "1/√μ (deep water case)"
 			else
-				info *= "└─No dealiasing. "
+				ν = 1
+				nu = "1 (shallow water case)"
 			end
-			info *= "\nDiscretized with $(mesh.N) collocation points on [$(mesh.xmin), $(mesh.xmax)]."
+		else
+			ν = param.ν
+			nu = "$ν"
+		end
+		if μ == Inf || ν==0 || IL == true # infinite layer case
+			IL = true;  # IL (=Infinite layer) is a flag to be used thereafter
+			μ = 1; ν = 1; # Then we should set μ=ν=1 in subsequent formula.
+		end
+		mesh = Mesh(param)
+
+		# Print information
+		info = "Matsuno model.\n"
+		if IL == true
+			info *= "├─Steepness parameter ϵ=$ϵ (infinite depth case).\n"
+		else
+			info *= "├─Shallowness parameter μ=$μ, nonlinearity parameter ϵ=$ϵ, \
+					scaling parameter ν=$nu.\n"
+		end
+		if dealias == true || dealias == 1
+			info *= "└─Dealiasing with Orszag’s 3/2 rule. "
+		else
+			info *= "└─No dealiasing. "
+		end
+		info *= "\nDiscretized with $(mesh.N) collocation points on [$(mesh.xmin), $(mesh.xmax)]."
 
 		# Pre-allocate useful data
-        x        = mesh.x
-		k 		 = mesh.k
-        Γ        = abs.(k)
-        Dx       =  1im * k        	# Differentiation
-        H        = -1im * sign.(k) 	# Hilbert transform
-		if dealias == true || dealias == 1
-			Π⅔    = Γ .< (mesh.kmax-mesh.kmin)/3    # Dealiasing low-pass filter
+		x = mesh.x
+		k = mesh.k
+		if IL == true
+			Tμ 	= -1im * sign.(k)
+			G₀ 	= abs.(k)
 		else
-			Π⅔    = zero(Γ) .+ 1     		# No dealiasing (Π⅔=Id)
+			Tμ 	= -1im* sign.(k) .* tanh.(sqrt(μ)*abs.(k))
+			G₀ 	= sqrt(μ)*abs.(k).*tanh.(sqrt(μ)*abs.(k))
+		end
+		∂ₓ	=  1im * sqrt(μ)* k            # Differentiation
+		if dealias == true || dealias == 1
+			Π⅔    = abs.(k) .< (mesh.kmax-mesh.kmin)/3    # Dealiasing low-pass filter
+		else
+			Π⅔    = zero(k) .+ 1     		# No dealiasing (Π⅔=Id)
 		end
 
         hnew = zeros(Complex{Float64}, mesh.N)
@@ -56,13 +85,13 @@ mutable struct Matsuno_fast <: AbstractModel
 		function f!(U)
 
 		    for i in eachindex(hnew)
-		        hnew[i] = Γ[i] * U[i,1]
+		        hnew[i] = G₀[i] * U[i,1]
 		    end
 
 		    ldiv!(unew, Px, hnew )
 
 		    for i in eachindex(hnew)
-		        hnew[i] = Dx[i] * U[i,1]
+		        hnew[i] = ∂ₓ[i] * U[i,1]
 		    end
 
 		    ldiv!(I₁, Px, hnew)
@@ -81,11 +110,11 @@ mutable struct Matsuno_fast <: AbstractModel
 
 		    mul!(I₃, Px, I₂)
 
-		    I₃    .*= Dx
+		    I₃    .*= ∂ₓ
 
-		    for i in eachindex(H)
-		        U[i,1]  = H[i] * U[i,2]
-		        I₀[i] = Γ[i] * U[i,2]
+		    for i in eachindex(Tμ)
+		        U[i,1]  = Tμ[i] * U[i,2]
+		        I₀[i] = G₀[i] * U[i,2]
 		    end
 
 		    ldiv!(I₂, Px, I₀)
@@ -94,24 +123,25 @@ mutable struct Matsuno_fast <: AbstractModel
 
 		    mul!(hnew, Px, I₂)
 
-		    hnew  .*= H
+		    hnew  .*= Tμ
 		    I₃    .+= hnew
 		    I₃    .*= ϵ .* Π⅔
 
 		    for i in eachindex(I₃)
 		        U[i,1] -= I₃[i]
 		    end
+			U[:,1] ./= sqrt(μ)/ν
 
 		    I₃    .=  unew.^2
 
 		    mul!(unew, Px, I₃)
 
-		    unew  .*= Dx
-		    unew  .*= ϵ/2 .* Π⅔
+		    unew  .*= ∂ₓ
+		    unew  .*= ϵ/2/ν .* Π⅔
 		    I₁    .-= unew
 
 		    for i in eachindex(I₁)
-		        U[i,2] =  I₁[i]
+		        U[i,2] =  I₁[i]/sqrt(μ)
 		    end
 
 		end
@@ -120,7 +150,7 @@ mutable struct Matsuno_fast <: AbstractModel
 		function mapto(data::InitialData)
 			fftη = Π⅔ .* fft(data.η(x));
 			fftv = Π⅔ .* fft(data.v(x));
-			U = [fftη fftv-ϵ* Π⅔ .*fft(ifft(H.*fftv).*ifft(Dx.*fftη) )]
+			U = [fftη fftv-ϵ* Π⅔ .*fft(ifft(Tμ.*fftv).*ifft(∂ₓ.*fftη) )]
 			return U
 		end
 
@@ -128,8 +158,8 @@ mutable struct Matsuno_fast <: AbstractModel
 		# - `η` is the surface deformation;
 		# - `v` is the velocity variable.
 		function mapfro(U)
-			#real(ifft(view(U,:,1))),real(ifft(view(U,:,2))+ϵ* ifft(H.*view(U,:,2)).*ifft(Dx.*view(U,:,1)))
-			real(ifft(U[:,1])),real(ifft(U[:,2])+ϵ* ifft(H.*U[:,2]).*ifft(Dx.*U[:,1]))
+			#real(ifft(view(U,:,1))),real(ifft(view(U,:,2))+ϵ* ifft(Tμ.*view(U,:,2)).*ifft(∂ₓ.*view(U,:,1)))
+			real(ifft(U[:,1])),real(ifft(U[:,2])+ϵ* ifft(Tμ.*U[:,2]).*ifft(∂ₓ.*U[:,1]))
 
 		end
 
@@ -138,17 +168,19 @@ mutable struct Matsuno_fast <: AbstractModel
 end
 
 """
-	Matsuno(param;dealias,label)
+	Matsuno(param;kwargs)
 
 Define an object of type `AbstractModel` in view of solving the initial-value problem for
 the quadratic deep-water model proposed by [Matsuno](https://doi.org/10.1103/PhysRevLett.69.609).
 
-# Arguments
+# Argument
 `param` is of type `NamedTuple` and must contain
-- the dimensionless parameters `ϵ` (nonlinearity);
-- numerical parameters to construct the mesh of collocation points as `mesh = Mesh(param)`.
+- dimensionless parameters `ϵ` (nonlinearity) and `μ` (dispersion);
+- optionally, `ν` the shallow/deep water scaling factor. By default, `ν=1` if `μ≦1` and `ν=1/√μ` otherwise. Set the infinite-layer case if `ν=0`, or `μ=Inf`.
+- numerical parameters to construct the mesh of collocation points as `mesh = Mesh(param)`
 
 ## Optional keyword arguments
+- `IL`: Set the infinite-layer case if `IL=true` (or `μ=Inf`, or `ν=0`), in which case `ϵ` is the steepness parameter. Default is `false`.
 - `dealias`: dealiasing with `1/3` Orlicz rule if `true` or no dealiasing if `false` (by default);
 - `label`: a label for future references (default is `"Matsuno"`);
 
@@ -171,15 +203,39 @@ mutable struct Matsuno <: AbstractModel
 
 
     function Matsuno(param::NamedTuple ;
-						dealias=false, label = "Matsuno" )
+						IL	    = false,
+						dealias=false,
+						label = "Matsuno" )
 
 		# Set up
+		μ 	= param.μ
 		ϵ 	= param.ϵ
+		if !in(:ν,keys(param))
+			if μ > 1
+				ν = 1/sqrt(μ)
+				nu = "1/√μ (deep water case)"
+			else
+				ν = 1
+				nu = "1 (shallow water case)"
+			end
+		else
+			ν = param.ν
+			nu = "$ν"
+		end
+		if μ == Inf || ν==0 || IL == true # infinite layer case
+			IL = true;  # IL (=Infinite layer) is a flag to be used thereafter
+			μ = 1; ν = 1; # Then we should set μ=ν=1 in subsequent formula.
+		end
 		mesh = Mesh(param)
 
 		# Print information
 		info = "Matsuno model.\n"
-		info *= "├─Steepness parameter ϵ=$ϵ (infinite depth case).\n"
+		if IL == true
+			info *= "├─Steepness parameter ϵ=$ϵ (infinite depth case).\n"
+		else
+			info *= "├─Shallowness parameter μ=$μ, nonlinearity parameter ϵ=$ϵ, \
+					scaling parameter ν=$nu.\n"
+		end
 		if dealias == true || dealias == 1
 			info *= "└─Dealiasing with Orszag’s 3/2 rule. "
 		else
@@ -188,15 +244,21 @@ mutable struct Matsuno <: AbstractModel
 		info *= "\nDiscretized with $(mesh.N) collocation points on [$(mesh.xmin), $(mesh.xmax)]."
 
 		# Pre-allocate useful data
-		x   = mesh.x
-		k 	= mesh.k
-        Γ 	= abs.(k)
-    	∂ₓ	=  1im * k            # Differentiation
-        H 	= -1im * sign.(k)     # Hilbert transform
-		if dealias == true || dealias == 1
-			Π⅔    = Γ .< (mesh.kmax-mesh.kmin)/3     # Dealiasing low-pass filter
+		x = mesh.x
+		k = mesh.k
+		if IL == true
+			Tμ 	= -1im * sign.(k)
+			G₀ 	= abs.(k)
 		else
-			Π⅔    = zero(Γ) .+ 1     # Dealiasing low-pass filter
+			Tμ 	= -1im* sign.(k) .* tanh.(sqrt(μ)*abs.(k))
+			G₀ 	= sqrt(μ)*abs.(k).*tanh.(sqrt(μ)*abs.(k))
+		end
+		∂ₓ	=  1im * sqrt(μ)* k            # Differentiation
+
+		if dealias == true || dealias == 1
+			Π⅔    = abs.(k) .< (mesh.kmax-mesh.kmin)/3 	# Dealiasing low-pass filter
+		else
+			Π⅔    = zero(k) .+ 1     		# No dealiasing (Π⅔=Id)
 		end
 
         hnew = zeros(Complex{Float64}, mesh.N)
@@ -211,12 +273,12 @@ mutable struct Matsuno <: AbstractModel
 
 		   hnew .= ifft(U[:,1])
 		   unew .= ifft(U[:,2])
-		   I₃ .= fft(ifft((∂ₓ).*U[:,1]).*ifft((Γ).*U[:,1]))
-		   I₁ .= H.*U[:,2].-ϵ*Π⅔.*(H.*fft(hnew.*ifft(Γ.*U[:,2])).+∂ₓ.*fft(hnew.*unew))
-		   I₂ .= -(∂ₓ.*U[:,1])+ϵ*Π⅔.*(I₃-∂ₓ.*fft(unew.^2)/2)
+		   I₃ .= fft(ifft(∂ₓ.*U[:,1]).*ifft(G₀.*U[:,1]))
+		   I₁ .= Tμ.*U[:,2].-ϵ*Π⅔.*(Tμ.*fft(hnew.*ifft(G₀.*U[:,2])).+∂ₓ.*fft(hnew.*unew))
+		   I₂ .= -ν*(∂ₓ.*U[:,1])+ϵ*Π⅔.*(ν*I₃-∂ₓ.*fft(unew.^2)/2)
 		   #
-		   U[:,1] .= I₁
-		   U[:,2] .= I₂
+		   U[:,1] .= I₁/sqrt(μ)/ν
+		   U[:,2] .= I₂/sqrt(μ)/ν
 
 		end
 
@@ -224,7 +286,7 @@ mutable struct Matsuno <: AbstractModel
 		function mapto(data::InitialData)
 			fftη = Π⅔ .* fft(data.η(x));
 			fftv = Π⅔ .* fft(data.v(x));
-			U = [fftη fftv-ϵ* Π⅔ .*fft(ifft(H.*fftv).*ifft(∂ₓ.*fftη) )]
+			U = [fftη fftv-ϵ* Π⅔ .*fft(ifft(Tμ.*fftv).*ifft(∂ₓ.*fftη) )]
 			return U
 		end
 
@@ -232,7 +294,7 @@ mutable struct Matsuno <: AbstractModel
 		# - `η` is the surface deformation;
 		# - `v` is the velocity variable.
 		function mapfro(U)
-			real(ifft(U[:,1])),real(ifft(U[:,2])+ϵ* ifft(H.*U[:,2]).*ifft(∂ₓ.*U[:,1]))
+			real(ifft(U[:,1])),real(ifft(U[:,2])+ϵ* ifft(Tμ.*U[:,2]).*ifft(∂ₓ.*U[:,1]))
 		end
 
 		new(label, f!, mapto, mapfro, info )
