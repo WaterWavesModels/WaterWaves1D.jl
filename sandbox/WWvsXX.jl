57af6dbfa083da55d277485b61c3abd451ee2b91
Vincent Duchêne <vyb.duchene@gmail.com>
2023-05-05 15:36:28 +0200

    Update WWvsXX.jl
    Whitham model

--- sandbox/WWvsXX.jl
+++ sandbox/WWvsXX.jl
@@ -10,11 +10,11 @@ using WaterWaves1D;
 #include("../src/dependencies.jl")
 
 #---- parameters
-param = ( μ  = .1,
-			ϵ  = 1,
+param = ( μ  = 0.1,
+			ϵ  = 0.05,
         	N  = 2^12, 	# number of collocation points
-            L  = 10,	# size of the mesh (-L,L)
-            T  = 5,		# final time of computation
+            L  = 15,	# size of the mesh (-L,L)
+            T  = 10,		# final time of computation
             dt = 0.001, # timestep
 			ns = 50,   	# data stored every ns step
 				);
@@ -26,8 +26,14 @@ init = Init(g,z);
 #---- models to compare
 models=AbstractModel[]
 push!(models,WaterWaves(param))
-push!(models,PseudoSpectral(param;order=2,dealias=1,lowpass=1/100))
-push!(models,PseudoSpectral(param;order=3,dealias=1,lowpass=1/100))
+push!(models,Whitham(param))
+push!(models,Whitham(param;BBM=true))
+push!(models,WhithamBoussinesq(param))
+
+
+#push!(models,PseudoSpectral(param;order=2,dealias=1,lowpass=1/100))
+#push!(models,PseudoSpectral(param;order=3,dealias=1,lowpass=1/100))
+
 problems = Problem[]
 for model in models
 	push!(problems, Problem(model, init, param) )
@@ -40,11 +46,20 @@ end
 
 #---- visualization
 #include("../src/Figures.jl")
-#using Plots
-#gr()
+using Plots
+gr()
 
-plt = plot_solution(problems)
+plt = plot(problems;xlims=(0,15),legend=:topleft)
 display(plt)
 savefig("WWvsXX.pdf")
-anim = create_animation(problems)
-gif(anim, "WWvsXX.gif", fps=15)
+
+plot(problems[1:2];color=:2,xlims=(0,15),var=:difference)
+plot!(problems[[1 3]];color=:3,xlims=(0,15),var=:difference)
+plot!(problems[[1 4]];color=:4,xlims=(0,15),var=:difference)
+savefig("differences.pdf")
+
+
+anim = @animate for time in LinRange(0,param.T,101)
+    plot(problems,T=time,ylims=(-0.2,1))
+end
+gif(anim,"WWvsXX.gif",fps=15)
