911bd82c0cf4e488b939810f10aaa75c885f7295
Vincent Duchêne <vyb.duchene@gmail.com>
2025-08-22 17:06:17 +0200

    Update Choi.jl
    Comment

--- src/models/Choi.jl
+++ src/models/Choi.jl
@@ -4,7 +4,7 @@ export Choi
     Choi(param;kwargs)
 
 Define an object of type `AbstractModel` in view of solving the initial-value problem for
-the model proposed by [Choi](https://doi.org/10.1017/jfm.2022.544).
+(an asymptotically equivalent variant of) the model proposed by [Choi](https://doi.org/10.1017/jfm.2022.544).
 
 # Argument
 `param` is of type `NamedTuple` and must contain
